12ed1069c122710d75c167b89443c56cf1ae0801
echobin23 <league@WTS20.attlocal.net>
2018-02-13 17:42:55 -0800

    obediantrobot

--- src/extras/ObedientRobot.java
+++ src/extras/ObedientRobot.java
@@ -1,36 +1,54 @@
 package extras;
 
+import java.awt.Color;
+
 import javax.swing.JOptionPane;
 
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
 	public static void main(String[] args) {
+		bob.penDown();
+		bob.setSpeed(20);
+		String color = JOptionPane.showInputDialog("what color do you want the shape to be");
+		if(color.equalsIgnoreCase("BLUE")) {
+			bob.setPenColor(Color.BLUE);
+		}
+		else if(color.equalsIgnoreCase("red")) {
+			bob.setPenColor(Color.RED);
+		}
+		else if(color.equalsIgnoreCase("green")) {
+			bob.setPenColor(Color.GREEN);
+		}
+		else {
+			JOptionPane.showMessageDialog(null, color+ " not applicable");
+		}
 		String answer = JOptionPane.showInputDialog("What shape do you want");
-		if(answer.equals("square")) {
+		if(answer.equalsIgnoreCase("square")) {
 			drawSquare();
 		}
-		else if(answer.equals("triangle")) {
+		else if(answer.equalsIgnoreCase("triangle")) {
 			drawTriangle();
 		}
-		else if(answer.equals("circle")) {
+		else if(answer.equalsIgnoreCase("circle")) {
 			drawCircle();
+		}
+		else {
+			JOptionPane.showMessageDialog(null, answer+ " not applicable");
 		}
 		
 	}
 	static Robot bob = new Robot();
 	static void drawSquare() {
-		bob.penDown();
-		bob.setSpeed(10);
 		for(int i=0 ;i< 4; i++) {
 			bob.move(80);
 			bob.turn(90);
 		}
 	}
 	static void drawTriangle() {
-		for(int i=0 ;i< 3; i++) {
-	bob.turn(40);
-	bob.move(90);
+		for(int i=0 ;i< 4; i++) {
+		bob.move(90);
+		bob.turn(60);
 		}
 	}
 	static void drawCircle() {
